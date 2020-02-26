b235448a7e31080a1451d0b37349a0a1edf21dd9
Brian <sychu@yahoo.com>
2020-02-25 18:30:23 -0800

    init

---
+++ app/src/main/java/net/brianchu/testactivity/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.brianchu.testactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
